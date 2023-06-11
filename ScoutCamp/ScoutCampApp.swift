be3d0499ad77d891d488de6f5402dd08ca6edc77
Igor Sokół <isokol@freeportmetrics.com>
2023-06-11 21:54:55 +0200

    Initial Commit

---
+++ ScoutCamp/ScoutCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoutCampApp.swift
+//  ScoutCamp
+//
+//  Created by Igor SOKÓŁ on 11/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct ScoutCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
